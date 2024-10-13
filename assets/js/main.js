92b91392ff5ba165c77740bf0b49a3fe8b039487
rimardev <catamarce.bs@gmail.com>
2024-10-13 08:54:09 -0300

    agrego logica de Tarea 2 --ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,20 @@ function tarea1() {
     btn01.innerHTML = "- Borde";
   };  
 };
+
+function tarea2() {
+  let input1 = Number(document.querySelector("#input1").value);
+  let input2 = Number(document.querySelector("#input2").value);
+  let input3 = Number(document.querySelector("#input3").value);
+  let msg = document.querySelector(".msg");
+  if (input1 < 0 || input2 < 0 || input3 < 0){    
+    msg.innerHTML = "Los numeros NO PUEDEN ser Negativos";
+  } else {
+    let suma = input1 + input2 + input3;
+    if (suma > 10){
+      msg.innerHTML = "Llevas demasiados stickers";
+    } else {
+      msg.innerHTML = "Llevas " + suma + " stickers";
+    };
+  };  
+};
