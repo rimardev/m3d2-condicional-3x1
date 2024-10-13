38551ffd7db302942e00d7354c6b23276cf97bf5
rimardev <catamarce.bs@gmail.com>
2024-10-13 09:37:13 -0300

    agrego extructura HTML y estilos CSS para Tarea 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,15 +15,15 @@ function tarea2() {
   let input1 = Number(document.querySelector("#input1").value);
   let input2 = Number(document.querySelector("#input2").value);
   let input3 = Number(document.querySelector("#input3").value);
-  let msg = document.querySelector(".msg");
+  let msg2 = document.querySelector("#msg2");
   if (input1 < 0 || input2 < 0 || input3 < 0){    
-    msg.innerHTML = "Los numeros NO PUEDEN ser Negativos";
+    msg2.innerHTML = "Los numeros NO PUEDEN ser Negativos";
   } else {
     let suma = input1 + input2 + input3;
     if (suma > 10){
-      msg.innerHTML = "Llevas demasiados stickers";
+      msg2.innerHTML = "Llevas demasiados stickers";
     } else {
-      msg.innerHTML = "Llevas " + suma + " stickers";
+      msg2.innerHTML = "Llevas " + suma + " stickers";
     };
   };  
 };
